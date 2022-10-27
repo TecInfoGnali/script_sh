e54b08474381a81a06330b3fedcc15f6e7d96a1a
TecInfoGnali <tecinfo.gnali@gmail.com>
2022-10-27 17:34:43 -0300

    Atualização automática

---
+++ Trabalho/startup.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+google-chrome --new-window --pinned-tab-count=1 https://estudante.estacio.br/disciplinas
+google-chrome --new-window --pinned-tab-count=2 /home/gabriel_aquino/Documentos/workspace/Estacio_Materia_Banco_de_Dados/README.md https://liveestacio-my.sharepoint.com/personal/202101193369_alunos_estacio_br/_layouts/15/onedrive.aspx?id=%2Fpersonal%2F202101193369_alunos_estacio_br%2FDocuments%2FSistemas%20de%20informação%20-%20Linked%20Files%2FBanco%20de%20Dados&ga=1
+opera --new-window --pinned-tab-count=1 http://127.0.0.1/pgadmin4/login
