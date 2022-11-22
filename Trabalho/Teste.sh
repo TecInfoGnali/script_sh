df84154285c2f690745f154e3440257618b8a6a6
TecInfoGnali <tecinfo.gnali@gmail.com>
2022-11-22 16:03:40 -0300

    Autonomeação 'Teste.sh'

---
+++ Trabalho/Teste.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/bash
+xdotool mousemove 472 25 click 1
+sleep 0.1
+xdotool key Up
+sleep 0.1
+xdotool key F2
+sleep 0.1
+xdotool key Left
+sleep 0.1
+xdotool type "t6-m3-v-"
+sleep 0.1
+xdotool key Home
+sleep 0.1
+xdotool key "ctrl+Right"
+sleep 0.1
+xdotool key "ctrl+Right"
+
+# sleep 0.1
+# xdotool type "1" 
+
+sleep 0.1
+xdotool key "ctrl+Right"
