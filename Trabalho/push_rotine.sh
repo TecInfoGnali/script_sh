6393099e12641b870ca23e84a4f52110f8759965
TecInfoGnali <tecinfo.gnali@gmail.com>
2022-11-29 16:41:37 -0300

    Atualização automática

--- Trabalho/push_rotine.sh
+++ Trabalho/push_rotine.sh
@@ -12,27 +12,27 @@ xdotool mousemove 2884 964 click 1; sleep 0.5
 # xdotool type 'cd ..'; sleep 0.3
 # xdotool key KP_Enter; sleep 0.3
 
-xdotool type 'cd estacio-teoria-geral-sistemas/'; sleep 0.3
-xdotool key KP_Enter; sleep 0.3
-xdotool type 'git add .'; sleep 0.3
-xdotool key KP_Enter; sleep 0.3
-xdotool type 'git commit -m "Atualização automática"'; sleep 0.3
-xdotool key KP_Enter; sleep 0.3
-xdotool type 'git push origin main'; sleep 0.3
-xdotool key KP_Enter; sleep 0.3
-xdotool type 'cd ..'; sleep 0.3
-xdotool key KP_Enter; sleep 0.3
+# xdotool type 'cd estacio-teoria-geral-sistemas/'; sleep 0.3
+# xdotool key KP_Enter; sleep 0.3
+# xdotool type 'git add .'; sleep 0.3
+# xdotool key KP_Enter; sleep 0.3
+# xdotool type 'git commit -m "Atualização automática"'; sleep 0.3
+# xdotool key KP_Enter; sleep 0.3
+# xdotool type 'git push origin main'; sleep 0.3
+# xdotool key KP_Enter; sleep 0.3
+# xdotool type 'cd ..'; sleep 0.3
+# xdotool key KP_Enter; sleep 0.3
 
-xdotool type 'cd estudo-estacio/'; sleep 0.3
-xdotool key KP_Enter; sleep 0.3
-xdotool type 'git add .'; sleep 0.3
-xdotool key KP_Enter; sleep 0.3
-xdotool type 'git commit -m "Atualização automática"'; sleep 0.3
-xdotool key KP_Enter; sleep 0.3
-xdotool type 'git push origin main'; sleep 0.3./
-xdotool key KP_Enter; sleep 0.3
-xdotool type 'cd ..'; sleep 0.3
-xdotool key KP_Enter; sleep 0.3
+# xdotool type 'cd estudo-estacio/'; sleep 0.3
+# xdotool key KP_Enter; sleep 0.3
+# xdotool type 'git add .'; sleep 0.3
+# xdotool key KP_Enter; sleep 0.3
+# xdotool type 'git commit -m "Atualização automática"'; sleep 0.3
+# xdotool key KP_Enter; sleep 0.3
+# xdotool type 'git push origin main'; sleep 0.3
+# xdotool key KP_Enter; sleep 0.3
+# xdotool type 'cd ..'; sleep 0.3
+# xdotool key KP_Enter; sleep 0.3
 
 xdotool type 'cd script_sh/'; sleep 0.3
 xdotool key KP_Enter; sleep 0.3
@@ -40,7 +40,7 @@ xdotool type 'git add .'; sleep 0.3
 xdotool key KP_Enter; sleep 0.3
 xdotool type 'git commit -m "Atualização automática"'; sleep 0.3
 xdotool key KP_Enter; sleep 0.3
-xdotool type 'git push origin main'; sleep 0.3./
+xdotool type 'git push origin main'; sleep 0.3
 xdotool key KP_Enter; sleep 0.3
 xdotool type 'cd ..'; sleep 0.3
 xdotool key KP_Enter; sleep 0.3
